d0ff1362108fc33fdbfa82c784df6e0f10471978
Gustavo <gustavo.rocha18@fatec.sp.gov.br>
2025-08-13 15:13:47 -0300

    Movendo arquivos
    movendo arquivos para uma melhor organização

---
+++ Dev Web III/Aula/Projeto-SpringInitializr/src/test/java/br/gov/sp/cps/springinitializr/ProjetoSpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.cps.springinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
